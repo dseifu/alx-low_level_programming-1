ddf15362b29f88574866b2a1a7e927c4d2928f08
“SabaNatanem” <sabainaab@gmail.com>
2022-10-14 14:45:44 +0000

    fix for positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,17 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
-		printf("is positive");
+		printf("is positive\n");
 	}
-		if (n=0)
-		{
-			 printf("is positive");
-		}
-	if (n<0)
+	if (n == 0)
 	{
-		 printf("is positive");
+		printf("is zero\n");
+	}
+	if (n < 0)
+	{
+		printf("is negative\n");
 	}
 	return (0);
 }
