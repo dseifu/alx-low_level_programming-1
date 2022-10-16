4d530a74840b8930e4810f2154d6ef24eae47a8b
“SabaNatanem” <sabainaab@gmail.com>
2022-10-16 12:49:19 +0000

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,20 +12,20 @@ int main(void)
 	{
 		for (n2 = n1 + 1; n2 < 9; n2++)
 		{
-			for (n3 = n2 + 1; n3 < 10; n2++)
+			for (n3 = n2 + 1; n3 < 10; n3++)
 			{
 				putchar((n1 % 10) + '0');
 				putchar((n2 % 10) + '0');
 				putchar((n3 % 10) + '0');
 
-				if (n1 == 8 && n2 == 9 (&& n3 == 10)
+				if (n1 == 7 && n2 == 8 && n3 == 9)
 					continue;
 
 				putchar(',');
 				putchar(' ');
 			}
 		}
+	}
 		putchar('\n');
 		return (0);
-	}
 }
